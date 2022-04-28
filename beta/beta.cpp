6fa930b4076bddbf79a3f60d92bc17c3327f1047
Admin <Admin@ASUS-TUF-DASH-F>
2022-04-28 20:23:26 +0200

    Add project files.

---
+++ beta/beta.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int table_size = 20;
+	int table[table_size];
+	char i;
+	int zmienna;
+	char op;
+
+	for (i = 0; i < table_size; i++)
+	{
+		cout << "Podaj " << i + 1 << " liczbe, aby przerwac podaj 0  " << endl;
+
+		cin >> zmienna;
+
+		if (zmienna == 0)
+		{
+			break;
+		}
+		else
+		{
+			table[i] = zmienna;
+		}
+	}
+
+	cout << "podaj operacje" << endl;
+	cin >> op;
+
+	switch (op)
+	{
+	case '+':
+	{
+		int wynik = 0;
+		for (int j = 0; j < i; j++)
+		{
+			wynik += table[j];
+		}
+		cout << "wynik operacji " << op << ": " << wynik << endl;
+		break;
+	}
+	default:
+		cout << "podaj prawidlowy operator: " << endl;
+		break;
+	}
+
+	return 0;
+}
